643828c2461dd99294cb7a0bc511f2f31eb6491c
elijiah silvereagle <elijiahsilvereagle@gmail.com>
2024-04-18 14:30:09 -0700

    Int Test 6

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -59,65 +59,65 @@ export class Game extends Scene
 
         this.joystick = new Joystick(this, 150, this.camera.height * 0.7);
 
-        const resetButton = this.add.text(this.cameras.scene.scale.width * 0.7, window.innerHeight / 2 - 50, 'Reset [Bad]', {
-            fontFamily: 'Arial Black', fontSize: 24, color: '#ffffff', stroke: '#000000', strokeThickness: 6, align: 'center'
-        }).setDepth(100).setInteractive();
+        // const resetButton = this.add.text(this.cameras.scene.scale.width * 0.7, window.innerHeight / 2 - 50, 'Reset [Bad]', {
+        //     fontFamily: 'Arial Black', fontSize: 24, color: '#ffffff', stroke: '#000000', strokeThickness: 6, align: 'center'
+        // }).setDepth(100).setInteractive();
 
-        resetButton.on('pointerdown', () => {
-            this.joystick.resetPointer();
-        });
-        resetButton.on('pointerover', () => {
-            resetButton.setColor('#000000');
-            resetButton.setStroke('#fdf6d8', 6);
-        });
-        resetButton.on('pointerout', () => {
-            resetButton.setColor('#ffffff');
-            resetButton.setStroke('#000000', 6);
-        });
+        // resetButton.on('pointerdown', () => {
+        //     this.joystick.resetPointer();
+        // });
+        // resetButton.on('pointerover', () => {
+        //     resetButton.setColor('#000000');
+        //     resetButton.setStroke('#fdf6d8', 6);
+        // });
+        // resetButton.on('pointerout', () => {
+        //     resetButton.setColor('#ffffff');
+        //     resetButton.setStroke('#000000', 6);
+        // });
 
-        this.buttonText = this.add.text(this.cameras.scene.scale.width * 0.7 + 5, window.innerHeight - 140, 'Reset [Good]', {
-            fontFamily: 'Arial Black', fontSize: 24, color: '#ffc700', stroke: '#000000', strokeThickness: 6, align: 'center'
-        }).setDepth(101);
+        // this.buttonText = this.add.text(this.cameras.scene.scale.width * 0.7 + 5, window.innerHeight - 140, 'Reset [Good]', {
+        //     fontFamily: 'Arial Black', fontSize: 24, color: '#ffc700', stroke: '#000000', strokeThickness: 6, align: 'center'
+        // }).setDepth(101);
 
-        let button = {
-            border: new Phaser.GameObjects.Graphics(this),
-            graphic: new Phaser.GameObjects.Graphics(this),
-            x: this.cameras.scene.scale.width * 0.7,
-            y: window.innerHeight - 150,
-            height: 50,
-            width: window.innerWidth * 0.225,
-        };
+        // let button = {
+        //     border: new Phaser.GameObjects.Graphics(this),
+        //     graphic: new Phaser.GameObjects.Graphics(this),
+        //     x: this.cameras.scene.scale.width * 0.7,
+        //     y: window.innerHeight - 150,
+        //     height: 50,
+        //     width: window.innerWidth * 0.225,
+        // };
 
-        button.graphic.setInteractive(new Phaser.Geom.Rectangle(
-            button.x, button.y, 
-            button.width, button.height), 
-            Phaser.Geom.Rectangle.Contains)
-                .on('pointerdown', (_pointer: any, _localX: any, _localY: any, _event: any) => {
-                    // console.log('Reset [Good]');
-                    this.joystick.resetPointer();
+        // button.graphic.setInteractive(new Phaser.Geom.Rectangle(
+        //     button.x, button.y, 
+        //     button.width, button.height), 
+        //     Phaser.Geom.Rectangle.Contains)
+        //         .on('pointerdown', (_pointer: any, _localX: any, _localY: any, _event: any) => {
+        //             // console.log('Reset [Good]');
+        //             this.joystick.resetPointer();
 
-                })
-                .on('pointerover', () => {
-                    this.buttonText.setColor('#000000');
-                    this.buttonText.setStroke('#ffc700', 6);
-                })
-                .on('pointerout', () => {
-                    this.buttonText.setColor('#ffc700');
-                    this.buttonText.setStroke('#000000', 6);
-                });
+        //         })
+        //         .on('pointerover', () => {
+        //             this.buttonText.setColor('#000000');
+        //             this.buttonText.setStroke('#ffc700', 6);
+        //         })
+        //         .on('pointerout', () => {
+        //             this.buttonText.setColor('#ffc700');
+        //             this.buttonText.setStroke('#000000', 6);
+        //         });
 
-        button.graphic.setScrollFactor(0);
-        button.border.setScrollFactor(0);
-        button.graphic.setDepth(100);
-        button.border.setDepth(100);
+        // button.graphic.setScrollFactor(0);
+        // button.border.setScrollFactor(0);
+        // button.graphic.setDepth(100);
+        // button.border.setDepth(100);
 
-        this.button = button;
-        this.add.existing(button.graphic);
-        this.add.existing(button.border);
-        this.add.existing(resetButton);
+        // this.button = button;
+        // this.add.existing(button.graphic);
+        // this.add.existing(button.border);
+        // this.add.existing(resetButton);
 
         const { width, height } = this.camera;
-        const buttonX = width - 100;
+        const buttonX = width / 2;
         const buttonY = height * 0.85; // Negative sign for Y to start from top
         
         let circle: Button = {
